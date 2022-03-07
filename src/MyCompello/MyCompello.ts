67c7614b2f8b5617c21a657b9f52d9e74372664e
Ian <iwhite99@protonmail.com>
2022-03-07 13:11:53 -0600

    feat(mycompello): Update Available Methods & Workflows (#1)
    * Update workflows
    
    * Update MyCompello methods

---
+++ src/MyCompello/MyCompello.ts
@@ -0,0 +1,266 @@
+import SftpClient from 'ssh2-sftp-client';
+
+export interface ClientOptions {
+  host: string;
+  port: number;
+  username: string;
+  password: string;
+}
+
+export default class MyCompello {
+  private client: SftpClient;
+
+  private config: ClientOptions;
+
+  constructor(config: ClientOptions) {
+    this.config = config;
+    this.client = new SftpClient();
+  }
+
+  /**
+   * Initialize the SFTP client connection
+   * @returns Promise<SftpClient>
+   */
+  public async connect(): Promise<SftpClient> {
+    await this.client
+      .connect({
+        host: this.config.host,
+        port: this.config.port,
+        username: this.config.username,
+        password: this.config.password,
+      })
+      .catch((err: Error) => {
+        throw err;
+      });
+    return this.client;
+  }
+
+  /**
+   * Fetches a list of files from the provided path
+   * @param remotePath {string}
+   * @returns {Promise<SftpClient.FileInfo[]>}
+   */
+  public async list(remotePath: string): Promise<SftpClient.FileInfo[]> {
+    const client = await this.connect();
+    const files = await client.list(remotePath);
+    client.end();
+    return files;
+  }
+
+  /**
+   * Check if the provided file exists
+   * @param remotePath {string}
+   * @returns {Promise<string | boolean>}
+   */
+  public async exists(remotePath: string): Promise<string | boolean> {
+    const client = await this.connect();
+    const exists = await client.exists(remotePath);
+    client.end();
+    return exists;
+  }
+
+  /**
+   * Retrieve a file from path.
+   * @param remotePath {string}
+   * @param localPath {string | NodeJS.WritableStream | undefined}
+   * @param options {SftpClient.TransferOptions | undefined}
+   * @returns {Promise<string | Buffer | NodeJS.WritableStream>}
+   */
+  public async get(
+    remotePath: string,
+    localPath: string | NodeJS.WritableStream | undefined,
+    options?: SftpClient.TransferOptions,
+  ): Promise<string | Buffer | NodeJS.WritableStream> {
+    const client = await this.connect();
+    const file = await client.get(remotePath, localPath, options);
+    client.end();
+    return file;
+  }
+
+  /**
+   * Downloads a file at path to dst using parallel reads for faster throughput
+   * @param remotePath {string}
+   * @param localPath {string}
+   * @returns {Promise<string>}
+   */
+  public async fastGet(remotePath: string, localPath: string): Promise<string> {
+    const client = await this.connect();
+    const file = await client.fastGet(remotePath, localPath);
+    client.end();
+    return file;
+  }
+
+  /**
+   * Uploads a file to path
+   * @param localPath {string | Buffer | NodeJS.ReadableStream}
+   * @param remotePath {string}
+   * @param options {SftpClient.TransferOptions | undefined}
+   * @returns {Promise<string | Buffer | NodeJS.ReadableStream>}
+   */
+  public async put(
+    localPath: string | Buffer | NodeJS.ReadableStream,
+    remotePath: string,
+    options?: SftpClient.TransferOptions,
+  ): Promise<string | Buffer | NodeJS.ReadableStream> {
+    const client = await this.connect();
+    const file = await client.put(localPath, remotePath, options);
+    client.end();
+    return file;
+  }
+
+  /**
+   * Uploads a file to path using concurrency
+   * @param localPath {string}
+   * @param remotePath {string}
+   * @param options {SftpClient.TransferOptions | undefined}
+   * @returns
+   */
+  public async fastPut(
+    localPath: string,
+    remotePath: string,
+    options?: SftpClient.FastPutTransferOptions,
+  ): Promise<string | Buffer | NodeJS.ReadableStream> {
+    const client = await this.connect();
+    const file = await client.fastPut(localPath, remotePath, options);
+    client.end();
+    return file;
+  }
+
+  /**
+   * Append data to remote file.
+   * @param input {Buffer | NodeJS.ReadableStream}
+   * @param remotePath {string}
+   * @param options {SftpClient.TransferOptions | undefined}
+   * @returns {Promise<string>}
+   */
+  public async append(
+    input: Buffer | NodeJS.ReadableStream,
+    remotePath: string,
+    options?: SftpClient.WriteStreamOptions,
+  ): Promise<string | Buffer | NodeJS.ReadableStream> {
+    const client = await this.connect();
+    const file = await client.append(input, remotePath, options);
+    client.end();
+    return file;
+  }
+
+  /**
+   * Create a directory at path
+   * @param remotePath {string}
+   * @param recursive {boolean}
+   * @returns {Promise<string>}
+   */
+  public async mkdir(remotePath: string, recursive?: boolean): Promise<string> {
+    const client = await this.connect();
+    const result = await client.mkdir(remotePath, recursive);
+    client.end();
+    return result;
+  }
+
+  /**
+   * Removes a directory at path
+   * @param remotePath {string}
+   * @param recursive {boolean}
+   * @returns {Promise<string>}
+   */
+  public async rmdir(remotePath: string, recursive?: boolean): Promise<string> {
+    const client = await this.connect();
+    const result = await client.rmdir(remotePath, recursive);
+    client.end();
+    return result;
+  }
+
+  /**
+   * Delete a file on the remote server.
+   * @param remotePath {string}
+   * @param noError {boolean}
+   */
+  public async delete(remotePath: string, noError?: boolean): Promise<string> {
+    const client = await this.connect();
+    const result = await client.delete(remotePath, noError);
+    client.end();
+    return result;
+  }
+
+  /**
+   * Rename a file or directory.
+   * @param fromPath {string}
+   * @param toPath {string}
+   * @returns {Promise<string>}
+   */
+  public async rename(fromPath: string, toPath: string): Promise<string> {
+    const client = await this.connect();
+    const result = await client.rename(fromPath, toPath);
+    client.end();
+    return result;
+  }
+
+  /**
+   * Uses openSSH POSIX rename extension to rename a file or directory.
+   * @param fromPath {string}
+   * @param toPath {stirng}
+   * @returns {Promise<string>}
+   */
+  public async posixRename(fromPath: string, toPath: string): Promise<string> {
+    const client = await this.connect();
+    const result = await client.posixRename(fromPath, toPath);
+    client.end();
+    return result;
+  }
+
+  /**
+   * Change the mode (read, write or execution permssions) of a file.
+   * @param remotePath {string}
+   * @param mode {string}
+   * @returns {Promise<string>}
+   */
+  public async chmod(remotePath: string, mode: string): Promise<string> {
+    const client = await this.connect();
+    const result = await client.chmod(remotePath, mode);
+    client.end();
+    return result;
+  }
+
+  /**
+   * Upload the contents of a local file to a remote file.
+   * @param localPath {string}
+   * @param remotePath {string}
+   * @param filter {string | RegExp}
+   * @returns {Promise<string>}
+   */
+  public async uploadDir(
+    localPath: string,
+    remotePath: string,
+    filter?: string | RegExp,
+  ): Promise<string> {
+    const client = await this.connect();
+    const result = await client.uploadDir(localPath, remotePath, filter);
+    client.end();
+    return result;
+  }
+
+  /**
+   * Download the contents of a remote file to a local file path.
+   * @param localPath
+   * @param remotePath
+   * @param filter
+   * @returns {Promise<string>}
+   */
+  public async downloadDir(
+    remotePath: string,
+    localPath: string,
+    filter?: string | RegExp,
+  ): Promise<string> {
+    const client = await this.connect();
+    const result = await client.downloadDir(remotePath, localPath, filter);
+    client.end();
+    return result;
+  }
+
+  /**
+   * Terminate the connection to the remote server.
+   */
+  public async end(): Promise<void> {
+    this.client.end();
+  }
+}
